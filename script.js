628b284219c25e4d5390fd8c78327041b8bbfd8a
Abdoul Akano <akano1nassirou@gmail.com>
2023-10-28 10:46:09 -0400

    dom manipulation done(hamburger menu)

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-Links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
